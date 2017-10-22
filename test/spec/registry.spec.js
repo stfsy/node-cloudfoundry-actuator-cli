bd3a8b06273157965ece127f941bceb8f8070ac5
stfsy <s.pfaffel@gmail.com>
2017-10-22 22:41:27 +0200

    test: test info contributor registry

---
+++ test/spec/registry.spec.js
@@ -0,0 +1,36 @@
+'use strict'
+
+const resolve = require('path').resolve
+const expect = require('chai').expect
+
+const GitContributor = require(resolve('lib/contributors/git'))
+const NpmContributor = require(resolve('lib/contributors/npm'))
+const Registry = require(resolve('lib/registry'))
+
+describe('Registry', () => {
+    let registry = null
+    beforeEach(() => {
+        registry = new Registry()
+    })
+    describe('.getContributors', () => {
+        it('should return git and npm info contributor', () => {
+            const contributors = registry.getContributors(['git', 'build'])
+            expect(contributors.length).to.equal(2)
+            const filtered = contributors.filter(contributor => {
+                const name = contributor.getName()
+                return (name != 'git' && name != 'build')
+            })
+            expect(filtered.length).to.equal(0)
+        })
+        it('should return npm info contributor', () => {
+            const contributors = registry.getContributors(['build'])
+            expect(contributors.length).to.equal(1)
+            expect(contributors[0].getName()).to.equal('build')
+        })
+        it('should return git info contributor', () => {
+            const contributors = registry.getContributors(['git'])
+            expect(contributors.length).to.equal(1)
+            expect(contributors[0].getName()).to.equal('git')
+        })
+    })
+})
